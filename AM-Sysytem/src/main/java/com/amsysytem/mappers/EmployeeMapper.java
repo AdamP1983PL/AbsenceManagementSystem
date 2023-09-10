a96b7e7bdea313754af9cdbbed586ff612981749
Adam Podlaski <podlaski83@gmail.com>
2023-09-10 13:06:36 +0200

    EmployeeMapper ans RequestMapper added, MySQL pass deleted

---
+++ AM-Sysytem/src/main/java/com/amsysytem/mappers/EmployeeMapper.java
@@ -0,0 +1,35 @@
+package com.amsysytem.mappers;
+
+import com.amsysytem.dto.EmployeeDto;
+import com.amsysytem.entity.Employee;
+
+public class EmployeeMapper {
+
+    public static EmployeeDto mapToEmployeeDto(Employee employee) {
+        return new EmployeeDto(
+                employee.getId(),
+                employee.getFirstName(),
+                employee.getLastName(),
+                employee.getEmail(),
+                employee.getPassword(),
+                employee.getPhoneNumber(),
+                employee.getEmployeeRole(),
+                employee.getDueDays(),
+                employee.getRequest()
+        );
+    }
+
+    public static Employee mapToEmployee(EmployeeDto employeeDto) {
+        return new Employee(
+                employeeDto.getId(),
+                employeeDto.getFirstName(),
+                employeeDto.getLastName(),
+                employeeDto.getEmail(),
+                employeeDto.getPassword(),
+                employeeDto.getPhoneNumber(),
+                employeeDto.getEmployeeRole(),
+                employeeDto.getDueDays(),
+                employeeDto.getRequest()
+        );
+    }
+}
---
+++ AM-Sysytem/src/main/java/com/amsysytem/mappers/RequestMapper.java
@@ -0,0 +1,39 @@
+package com.amsysytem.mappers;
+
+import com.amsysytem.dto.RequestDto;
+import com.amsysytem.entity.Request;
+
+public class RequestMapper {
+
+    public static RequestDto mapToRequestDto(Request request) {
+        return new RequestDto(
+                request.getId(),
+                request.getLastName(),
+                request.getFirstName(),
+                request.getEmail(),
+                request.getDateTimeCreated(),
+                request.getStartingDate(),
+                request.getEndingDate(),
+                request.getDaysCount(),
+                request.getDueDays(),
+                request.getStatus(),
+                request.getEmployee()
+        );
+    }
+
+    public static Request mapToRequest(RequestDto requestDto) {
+        return new Request(
+                requestDto.getId(),
+                requestDto.getLastName(),
+                requestDto.getFirstName(),
+                requestDto.getEmail(),
+                requestDto.getDateTimeCreated(),
+                requestDto.getStartingDate(),
+                requestDto.getEndingDate(),
+                requestDto.getDaysCount(),
+                requestDto.getDueDays(),
+                requestDto.getStatus(),
+                requestDto.getEmployee()
+        );
+    }
+}
