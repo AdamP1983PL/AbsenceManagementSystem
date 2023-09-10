444fd61e6ddd3d6aa2f511c8398de90332dad73f
Adam Podlaski <podlaski83@gmail.com>
2023-09-10 13:18:05 +0200

    RequestService and EmployeeService initial methods added

--- AM-Sysytem/src/main/java/com/amsysytem/service/EmployeeService.java
+++ AM-Sysytem/src/main/java/com/amsysytem/service/EmployeeService.java
@@ -1,7 +1,20 @@
 package com.amsysytem.service;
 
+import com.amsysytem.dto.EmployeeDto;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public interface EmployeeService {
+
+    List<EmployeeDto> getAllEmployees();
+
+    void save(EmployeeDto employeeDto);
+
+    EmployeeDto getEmployeeDtoById(Long employeeDtoId);
+
+    void updateEmployee(EmployeeDto employeeDto);
+
+    void deleteEmployee(Long employeeDtoId);
 }
--- AM-Sysytem/src/main/java/com/amsysytem/service/RequestService.java
+++ AM-Sysytem/src/main/java/com/amsysytem/service/RequestService.java
@@ -1,7 +1,18 @@
 package com.amsysytem.service;
 
+import com.amsysytem.dto.RequestDto;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public interface RequestService {
+
+    List<RequestDto> getAllRequests();
+
+    void save(RequestDto requestDto);
+
+    void updateRequest(RequestDto requestDto);
+
+    void deleteRequest(Long id);
 }
