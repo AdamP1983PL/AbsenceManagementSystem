03c90d396951f4f5f05debcb55736a80e42a957c
Adam Podlaski <podlaski83@gmail.com>
2023-10-16 19:45:03 +0200

    User class created

---
+++ AM-Sysytem/src/main/java/com/amsysytem/entity/User.java
@@ -0,0 +1,25 @@
+package com.amsysytem.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Table(name = "users")
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "user_name")
+    private String userName;
+
+    @Column(name = "password")
+    private String password;
+}
