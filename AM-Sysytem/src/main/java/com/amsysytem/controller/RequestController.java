75685cf665261452e31c54cbcc686ab846e91bcf
Adam Podlaski <podlaski83@gmail.com>
2023-10-18 20:45:29 +0200

    requests.html added

--- AM-Sysytem/src/main/java/com/amsysytem/controller/RequestController.java
+++ AM-Sysytem/src/main/java/com/amsysytem/controller/RequestController.java
@@ -1,4 +1,24 @@
 package com.amsysytem.controller;
 
+import com.amsysytem.dto.RequestDto;
+import com.amsysytem.service.RequestServiceImpl;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+@AllArgsConstructor
 public class RequestController {
+
+    public final RequestServiceImpl requestServiceImpl;
+
+    @GetMapping("/requests")
+    public String listRequest(Model model){
+        List<RequestDto> requestDtoList = requestServiceImpl.getAllRequests();
+        model.addAttribute("requestDtoList", requestDtoList);
+        return "requests";
+    }
 }
--- AM-Sysytem/src/main/java/com/amsysytem/service/RequestServiceImpl.java
+++ AM-Sysytem/src/main/java/com/amsysytem/service/RequestServiceImpl.java
@@ -1,16 +1,28 @@
 package com.amsysytem.service;
 
 import com.amsysytem.dto.RequestDto;
+import com.amsysytem.entity.Request;
+import com.amsysytem.mappers.RequestMapper;
+import com.amsysytem.repositories.RequestRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
+@AllArgsConstructor
 public class RequestServiceImpl implements RequestService{
+
+    public final RequestRepository requestRepository;
 
     @Override
     public List<RequestDto> getAllRequests() {
-        return null;
+        List<Request> requests = requestRepository.findAll();
+        List<RequestDto> requestDtos = requests.stream()
+                .map((request) -> RequestMapper.mapToRequestDto(request))
+                .collect(Collectors.toList());
+        return requestDtos;
     }
 
     @Override
