c98ad33479df3576be055442bb2f469e37f8ad27
Adam Podlaski <podlaski83@gmail.com>
2023-11-11 21:52:57 +0100

    Railway configuration tests

--- AM-Sysytem/src/main/java/com/amsysytem/AmSysytemApplication.java
+++ AM-Sysytem/src/main/java/com/amsysytem/AmSysytemApplication.java
@@ -4,12 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
-@PropertySource("classpath:db.properties")
+//@PropertySource("classpath:db.properties")
 @SpringBootApplication
 public class AmSysytemApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AmSysytemApplication.class, args);
     }
+
 
 }
