0486c156667af2c9f658a602178d3f1cd8d460b3
Adam Podlaski <podlaski83@gmail.com>
2023-11-11 14:31:23 +0100

    Add leftover to repo

---
+++ AM-Sysytem/src/test/java/com/amsysytem/AmSysytemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amsysytem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmSysytemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
